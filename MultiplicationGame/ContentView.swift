3f1b3a6dccea26fda8edb80d8c48df7b4444bad1
mrhaffner <mrhaffner13@gmail.com>
2021-06-24 17:55:15 -0500

    moved state and functions around the views. Created bindings to share certain states.  Added logic to generateQuestions()

--- MultiplicationGame/ContentView.swift
+++ MultiplicationGame/ContentView.swift
@@ -11,6 +11,9 @@ struct SettingsView: View {
     @State private var numberQuestion = 0
     @State private var numberTables = 1
     
+    @Binding var gameRunning: Bool
+    @Binding var questions: [(Int, Int)]
+    
     let numberQuestions = ["5", "10", "20", "All"]
     
     var body: some View {
@@ -35,45 +38,76 @@ struct SettingsView: View {
             .navigationTitle("Multiplication Tables")
             .toolbar{
                 Button("Start Game!") {
-                    
+                    gameRunning = true
+                    generateQuestions()
+                    print(questions)
                 }
             }
+        }
+    }
+    
+    func generateQuestions() {
+        let columns = 1...numberTables
+        let rows = 1...12
+        var tempArray = [(Int,Int)]()
+        for x in columns {
+            for y in rows {
+                tempArray.append((x, y))
+            }
+        }
+        tempArray.shuffle()
+        let numQuestion = numberQuestions[numberQuestion]
+        if numQuestion == "All" {
+            questions = tempArray
+        } else if numQuestion == "20" && numberTables == 1 {
+            questions = tempArray
+        } else {
+            let startNum = Int(numQuestion)!
+            tempArray.removeSubrange(startNum...)
+            questions = tempArray
         }
     }
 }
 
 struct GameView: View {
-    var body: some View {
-        Text("Game view")
-    }
-}
-
-struct ContentView: View {
-    @State private var gameRunning = true
-    @State private var question = [(Double, Double)]()
-    @State private var currentQuestions = 0
+    @State private var exampleQuestion = [(1,1), (2,2)]
+    @State private var currentQuestion = 0
     @State private var score = 0
+    @State private var answer = ""
+    
+    @Binding var gameRunning: Bool
+    @Binding var questions: [(Int, Int)]
     
     var body: some View {
-        Group {
-            if gameRunning {
-                GameView()
-            } else {
-                SettingsView()
+        VStack {
+            Text("What is \(exampleQuestion[currentQuestion].0) x \(exampleQuestion[currentQuestion].1)?")
+        
+            TextField("Enter your answer", text: $answer)
+                .multilineTextAlignment(.center)
+                .keyboardType(.numberPad)
+            Button("Submit Answer") {
+                checkAnswer()
             }
         }
     }
     
-    func newGame() {
-        
-    }
-    
-    func generateQuestions() {
-        
-    }
-    
     func checkAnswer() {
         
+    }
+}
+
+struct ContentView: View {
+    @State private var gameRunning = false
+    @State private var questions = [(Int, Int)]()
+
+    var body: some View {
+        Group {
+            if gameRunning {
+                GameView(gameRunning: $gameRunning, questions: $questions)
+            } else {
+                SettingsView(gameRunning: $gameRunning, questions: $questions)
+            }
+        }
     }
 }
 
